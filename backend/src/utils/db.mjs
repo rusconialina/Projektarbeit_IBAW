76cecdb05e42c1f2713262dd6e1ba95542f85e8d
Alina <rusconialina@gmail.com>
2022-01-13 14:07:41 +0100

    .env

--- backend/src/utils/db.mjs
+++ backend/src/utils/db.mjs
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import ".../.env"
 
 dotenv.config();
 
 export default function connect() {
   try {
-    mongoose.connect('mongodb+srv://ibaw:Y12AyGPaltP8EEMK@bibliothek.ujnx7.mongodb.net/test', {
+    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
   } catch (e) {
